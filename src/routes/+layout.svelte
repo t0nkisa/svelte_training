ee98a5e6f1cf8e833dc224e5e32f9c66d63671ff
t0nkisa <128639796+t0nkisa@users.noreply.github.com>
2024-01-24 23:25:22 +0100

    log check for web preview

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,12 @@
     import "../app.css";
     import type { LayoutServerData } from './$types';
     export let data: LayoutServerData;
+    console.log(data);
+    
 </script>
+
+<span>
+    {data.deploymentGitBranch}
+</span>
 
 <slot></slot>
