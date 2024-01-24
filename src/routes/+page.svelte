b49e7920f3cdd28ed21330f3283715dc8cec21e0
t0nkisa <128639796+t0nkisa@users.noreply.github.com>
2024-01-24 06:16:36 +0100

    setting up tailwind and daisyui

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,10 @@
 <h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<p class="text-red-500">Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<div class="card">
+    <div class="card-body">This is a card</div>
+    <div class="card-actions">
+        <button class="btn">Hello</button>
+        <button class="btn">Bye</button>
+    </div>
+</div>
