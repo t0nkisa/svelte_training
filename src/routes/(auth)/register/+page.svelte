980500d1fec5d5473ed883bd5e186eb1193b1b09
t0nkisa <128639796+t0nkisa@users.noreply.github.com>
2024-01-25 04:28:42 +0100

    auth group layout done

---
+++ src/routes/(auth)/register/+page.svelte
@@ -0,0 +1,18 @@
+<script>
+	import FormField from '$lib/components/form/FormField.svelte';
+	import FormButton from '$lib/components/form/FormButton.svelte';
+	import Link from '$lib/components/Link.svelte';
+	import Form from '$lib/components/form/Form.svelte';
+</script>
+
+<Form method="POST" action="?/register" title="Register">
+	<FormField name="username" label="Username" type="text" placeholder="John Doe" />
+	<FormField name="email" label="Email" type="email" placeholder="johndoe@gmail.com" />
+	<FormField name="password" label="Password" type="password" />
+	<FormField name="password_confirmation" label="Confirm Password" type="password" />
+
+	<div class="flex justify-around mt-4">
+		<Link href="/" content="Back" />
+		<FormButton content="Register" />
+	</div>
+</Form>
