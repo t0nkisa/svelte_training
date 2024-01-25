75e35f7fe92306e114805ac0aae230faa46ac880
t0nkisa <128639796+t0nkisa@users.noreply.github.com>
2024-01-25 03:16:04 +0100

    Link and Navbar components

---
+++ src/lib/components/Link.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	export let href: string;
+	export let content: string;
+</script>
+
+<a
+	{href}
+	class="block text-black_russian dark:text-gin px-4 py-2 dark:bg-golden_grass border-golden_grass border-8 dark:border-0 text-4xl font-extrabold dark:px-6 dark:py-4 hover:underline min-w-[200px] text-center"
+	>{content}</a
+>
