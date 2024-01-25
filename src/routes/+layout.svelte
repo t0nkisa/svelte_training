b8acfe27a272eb3243b1856934a183c2ec8053cd
t0nkisa <128639796+t0nkisa@users.noreply.github.com>
2024-01-25 03:18:09 +0100

    changed dark mode bg color

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,8 +2,8 @@
 	import '../app.css';
 </script>
 
-<div class="bg-gin-200 p-6">
-	<main class="min-h-dvh w-full flex flex-col bg-gin dark:bg-kimberly-950 p-4">
+<div class="bg-gin-200 dark:bg-kimberly-950 p-6">
+	<main class="min-h-dvh w-full flex flex-col bg-gin dark:bg-kimberly-900 p-4">
 		<slot />
 	</main>
 </div>
