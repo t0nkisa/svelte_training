c761506e84750b043e62d87daa8dce6eb43cbb2f
t0nkisa <128639796+t0nkisa@users.noreply.github.com>
2024-01-25 04:08:20 +0100

    added name to form field

--- src/lib/components/form/FormField.svelte
+++ src/lib/components/form/FormField.svelte
@@ -1,12 +1,14 @@
 <script lang="ts">
 	export let label: string;
 	export let type: string;
+	export let name: string;
 	export let placeholder: string = '';
 </script>
 
 <label class="flex flex-col text-black_russian dark:text-gin text-2xl">
 	{label}
 	<input
+		{name}
 		{type}
 		class="dark:text-gin dark:placeholder:text-gin/60 placeholder:text-black_russian/50 px-4 py-2 dark:bg-golden_grass border-golden_grass text-xl border-8 dark:border-0 font-extrabold dark:px-6 dark:py-4 min-w-[200px]"
 		{placeholder}
