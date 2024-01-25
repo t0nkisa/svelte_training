0d3b24f1880e58265cb5df2fcb05e672c34b5f95
t0nkisa <128639796+t0nkisa@users.noreply.github.com>
2024-01-25 03:16:18 +0100

    routing + layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,15 +1,9 @@
 <script lang="ts">
-    import "../app.css";
-    
-    
+	import '../app.css';
 </script>
 
-
-<main class="min-h-dvh flex flex-col">
-    <div class="flex-grow bg-primary">
-        <slot></slot>
-    </div>
-</main>
-
-
-
+<div class="bg-gin-200 p-6">
+	<main class="min-h-dvh w-full flex flex-col bg-gin dark:bg-kimberly-950 p-4">
+		<slot />
+	</main>
+</div>
