275d1fabae0e83b68aae508af220263b6d346feb
t0nkisa <128639796+t0nkisa@users.noreply.github.com>
2024-01-25 04:17:04 +0100

    added form

---
+++ src/lib/components/form/Form.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	export let method: string;
+	export let action: string;
+	export let title: string;
+</script>
+
+<form {method} {action} class="flex flex-col gap-4 w-1/3 h-1/2 m-auto p-8 dark:bg-kimberly-800">
+	<h1 class="text-4xl text-black_russian dark:text-gin self-center">{title}</h1>
+	<slot />
+</form>
