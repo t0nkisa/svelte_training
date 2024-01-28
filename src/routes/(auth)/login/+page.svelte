713405f4ff0ef4503505892f7a951d9f25d62ab8
t0nkisa <128639796+t0nkisa@users.noreply.github.com>
2024-01-28 23:43:00 +0100

    added link to go profile

--- src/routes/(auth)/login/+page.svelte
+++ src/routes/(auth)/login/+page.svelte
@@ -6,6 +6,13 @@
 	export let form;
 </script>
 
+{#if form?.success}
+	<div class="flex flex-col w-1/5 mx-auto">
+		Congratulations! You have successfully logged in.
+		<Link href="/profile" content="Go here to see your profile!" />
+	</div>
+{/if}
+
 <Form method="POST" action="?/login" title="Login">
 	<FormField name="email" label="Email" type="email" required={true} value={form?.email} />
 	<FormField name="password" label="Password" type="password" required={true} />
